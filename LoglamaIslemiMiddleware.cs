d17530f014ca523b1fc148f546247c8a18d1cce2
ontedi <universityofcu@gmail.com>
2022-01-18 02:53:45 +0300

    first commit

---
+++ LoglamaIslemiMiddleware.cs
@@ -0,0 +1,31 @@
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace AspNetCoreMiddleware
+{
+    public class LoglamaIslemiMiddleware
+    {
+        private readonly RequestDelegate _rdIstek;
+        public LoglamaIslemiMiddleware(RequestDelegate rdIstek)
+        {
+            _rdIstek = rdIstek;
+        }
+        public async Task Invoke(HttpContext httpContext)
+        {
+            string strIp = httpContext.Connection.RemoteIpAddress.ToString();
+            string strTarayiciBilgileri = httpContext.Request.Headers["User-Agent"];
+            string strAdresSatiri = httpContext.Request.QueryString.ToString();
+            string strPatika = "D:/calisma-masasi/dotnetcore/ornekler/AspNetCoreMiddleware/log.txt";
+            using (StreamWriter swYazici = System.IO.File.AppendText(strPatika))
+            {
+                swYazici.WriteLine("#### Request (BASLANGIC) ####");
+                swYazici.WriteLine("Ip Adresi: " + strIp);
+                swYazici.WriteLine("Tarayıcı Bilgileri: " + strTarayiciBilgileri);
+                swYazici.WriteLine("Adres Satiri: " + strAdresSatiri);
+                swYazici.WriteLine("#### Request (BITIS) ####");
+            }
+            await _rdIstek.Invoke(httpContext);
+        }
+    }
+}
